fae86690c4901856c94710d742ea03b8d591bb5b
Pkolli1s <pkolli1s@semo.edu>
2023-01-20 22:17:02 -0600

    Updated 11:16

--- JavaBasics/src/com/n2s/basics/Sample1.java
+++ JavaBasics/src/com/n2s/basics/Sample1.java
@@ -5,7 +5,27 @@ public class Sample1 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome");
+		System.out.println("args.length");
+		for(int i=0; i<args.length; i++)
+		{
+			System.out.println(args[i]);
+		}
+		
+		Sample1 Sample = new Sample1();
+		System.out.println(Sample.add(5, 8));
+		System.out.println(Sample.sub(5, 6));
+		
 
 	}
+     public int add(int x, int y)
+     {
+    	 return(x+y);
+     }
+     public int sub(int x, int y)
+     {
+    	 return(x-y);
+     }
+	
+	
 
 }
